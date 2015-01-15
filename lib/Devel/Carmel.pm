8eb07ef6fa53f7c8634b6d5c3e7628b7c02b0a71
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2015-01-14 22:24:18 -0800

    add Devel checker module that installs @INC hook to prevent loading modules outside Carmel

---
+++ lib/Devel/Carmel.pm
@@ -0,0 +1,58 @@
+package Devel::Carmel;
+use Module::CoreList;
+
+sub _find_border {
+    my($re, @entry) = @_;
+
+    my $in = 0;
+    for my $index (0..$#entry) {
+        my $entry = $entry[$index];
+        next if ref $entry;
+
+        if ($entry =~ $re) {
+            $in = 1, next;
+        } elsif ($in) {
+            return $index;
+        }
+    }
+
+    return;
+}
+
+# This assumes it's run under carmel exec
+my $base = $ENV{PERL_CARMEL_REPO} || "$ENV{HOME}/.cpanm/builds";
+my $index = _find_border qr!$base/.*?/blib/(lib|arch)?$!, @INC;
+
+if ($index) {
+    splice @INC, $index, 0, __PACKAGE__->new(@INC[0..$index-1]);
+}
+
+sub _package {
+    my $file = shift;
+    $file =~ s/\.pm$//;
+    $file =~ s!/!::!g;
+    $file;
+}
+
+sub new {
+    my($class, @inc) = @_;
+    bless { inc => \@inc }, shift;
+}
+
+sub Devel::Carmel::INC {
+    my($self, $file) = @_;
+
+    my @caller = caller 1;
+    if ($caller[3] =~ /^\(eval/ or defined $caller[6]) {
+        return;
+    }
+
+    my $mod = _package($file);
+    if ($Module::CoreList::version{$]+0}{$mod}) {
+        return;
+    }
+
+    die "Can't locate $file in \@INC in Carmel artifacts (You may need to install the $mod module with carmel install) (\@INC contains: @{$self->{inc}}).\n";
+}
+
+1;
