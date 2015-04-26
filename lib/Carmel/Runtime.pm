4aee7fa10d1b3d167dac0e3d01f0cea30ecca641
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2015-04-26 01:42:07 -0700

    replace Guard INC entry with dumbified site_perl a la lib::core::only.
    Guard was a bit too clever, and I don't like the fact it checks
    Module::CoreList on runtime, given CoreList will soon get a tie-based
    implementation.
    
    Now, remove site_perl altogether like lib::core::only, but allow
    Carmel::Preload and Module::Runtime as a whitelisted exception.
    
    This will hopefully have no issues with core modules with new versions
    installed into site_perl (hence the requirement of perl 5.12)

--- lib/Carmel/Runtime.pm
+++ lib/Carmel/Runtime.pm
@@ -1,28 +1,18 @@
 package Carmel::Runtime;
 use strict;
-
-sub _insert_before_sitelib {
-    my($inc) = @_;
-
-    require Config;
-    my %lib = map { $_ => 1 } @Config::Config{qw(sitearchexp archlibexp)};
-
-    my $index = -1;
-    for my $i (0..$#INC) {
-        $index = $i, last if $lib{$INC[$i]};
-    }
-
-    if ($index >= 0) {
-        splice @INC, $index, 0, $inc;
-    } else {
-        warn "Can't find \@INC entry for $Config::Config{sitearchexp}";
-    }
-}
+use Config;
 
 sub bootstrap {
     my($class, $modules, $inc) = @_;
 
-    _insert_before_sitelib(Carmel::Runtime::Guard->new);
+    my %allows = qw( Carmel/Preload.pm 1 Module/Runtime.pm 1 );
+    my %site   = ($Config{sitearchexp} => 1, $Config{sitelibexp} => 1);
+
+    for (@INC) {
+        $_ = Carmel::Runtime::SiteINC->new($_, \%allows)
+          if $site{$_};
+    }
+
     unshift @INC,
       Carmel::Runtime::FastINC->new(%$modules),
       @{$inc};
@@ -31,6 +21,27 @@ sub bootstrap {
 sub require_all {
     my($class, @phase) = @_;
     die "Deprecated. Use Carmel::Preload instead.";
+}
+
+package Carmel::Runtime::SiteINC;
+
+sub new {
+    my($class, $path, $allows) = @_;
+    bless {
+        path => $path,
+        allows => $allows,
+    }, $class;
+}
+
+sub Carmel::Runtime::SiteINC::INC {
+    my($self, $file) = @_;
+
+    if ($self->{allows}{$file}) {
+        open my $fh, '<', "$self->{path}/$file"
+          or return;
+        $INC{$file} = "$self->{path}/$file";
+        return $fh;
+    }
 }
 
 package Carmel::Runtime::FastINC;
@@ -44,64 +55,13 @@ sub Carmel::Runtime::FastINC::INC {
     my($self, $file) = @_;
 
     if ($self->{$file}) {
-        open my $fh, '<', $self->{$file}
-          or die "Can't load $self->{$file}: $!";
-        $INC{$file} = $self->{$file};
-        return $fh;
-    }
-}
-
-package Carmel::Runtime::Guard;
-
-# called in runtime via Carmel::Preload
-my %whitelist = (
-    'Carmel/Preload' => 1,
-    'Module/Runtime.pm' => 1,
-);
-
-sub new {
-    my $class = shift;
-    require Module::CoreList;
-    bless { corelist => {} }, $class;
-}
-
-sub _package {
-    my $file = shift;
-    $file =~ s/\.pm$//;
-    $file =~ s!/!::!g;
-    $file;
-}
-
-sub Carmel::Runtime::Guard::INC {
-    my($self, $file) = @_;
-
-    # Config_heavy.pl etc.
-    return if $file =~ /\.pl$/;
-
-    # whitelist
-    return if $whitelist{$file};
-
-    my $mod = _package($file);
-    my @caller = caller 0;
-
-    # eval { require Module }
-    return if $caller[3] =~ /^\(eval/ or defined $caller[6];
-
-    # core module calling another module is considered core, too.
-    # FIXME: Updated core module might call a non-core package
-    if ($self->{corelist}{$caller[0]}) {
-        $self->{corelist}{$mod} = 1;
+        if (open my $fh, '<', $self->{$file}) {
+            $INC{$file} = $self->{$file};
+            return $fh;
+        }
+        warn "Can't locate $file in $self->{$file}: $!";
         return;
     }
-
-    # core module
-    if ($Module::CoreList::version{$]+0}{$mod}) {
-        $self->{corelist}{$mod} = 1;
-        return;
-    }
-
-    # Module::Runtime::use_package_optimistically tries to parse this message.
-    die "Can't locate $file in \@INC (You may need to add `requires '$mod';` to your cpanfile and run carmel install) (\@INC contains: @INC) at $caller[1] line $caller[2].\n";
 }
 
 1;
