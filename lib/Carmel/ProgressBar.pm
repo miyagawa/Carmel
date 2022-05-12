3af45cfbbc54277ca69d7ffa6bfee9ac4b0d7ea7
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2022-05-11 20:33:20 -0700

    stop showing %, show actual number

--- lib/Carmel/ProgressBar.pm
+++ lib/Carmel/ProgressBar.pm
@@ -40,13 +40,14 @@ sub update {
     return if $self->quiet;
 
     my $width  = $self->width;
-    my $pct    = int(100 * $count / $self->total);
-    my $done   = ceil($pct * $width / 100);
+    my $done   = ceil($count * $width / $self->total);
     my $head   = $width == $done ? 0 : 1;
     my $remain = ($width - $done - $head);
+    my $len    = length $self->total;
 
-    my $line = sprintf "[%s%s%s] %3d%%",
-      ("=" x $done), (">" x $head), (" " x $remain), $pct;
+    my $line = sprintf "[%s%s%s] %${len}d/%d",
+      ("=" x $done), (">" x $head), (" " x $remain),
+      $count, $self->total;
 
     return if $self->_prev && $line eq $self->_prev;
 
@@ -58,7 +59,7 @@ sub update {
 
 sub clear {
     my $self = shift;
-    print "\r", " " x ($self->width + 7), "\r";
+    print "\r", " " x ($self->width + 2*length($self->total) + 4), "\r";
 }
 
 1;
