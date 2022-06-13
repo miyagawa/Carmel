5fdf0406ff44b75a0e0a8338e3b6f00ac01259c3
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2022-06-13 13:17:24 -0700

    fix URL for cpanm install

--- lib/Carmel.pm
+++ lib/Carmel.pm
@@ -150,7 +150,7 @@ stock perl.
   > git clone https://.../myapp.git && cd myapp
 
   # Install the modules to ./local (like carmel rollout)
-  > cpanm -L ./local --from $PWD/vendor/cache -nq --installdeps .
+  > cpanm -L ./local --from file://$PWD/vendor/cache -nq --installdeps .
 
 =head1 HOW IT WORKS
 
